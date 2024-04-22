fab33a88973eaf4c91dba07f267e42df100163f2
Craig Marais <craig.marais.85@gmail.com>
2024-04-22 13:04:56 +0200

    added success per year

--- SpaceX/Program.cs
+++ SpaceX/Program.cs
@@ -16,11 +16,31 @@ class Program
             from launch in launches
             where launch.success == false
             select launch;
+
+        int launch2011 =
+            launches
+                .Where(l => l.date_utc.Year == 2011)
+                .Count();
         
-        foreach (Launch launch in failures2)
-        {
-            Console.WriteLine($"{launch.name}\t\t{launch.date_utc.Year}\n" +
-                              $"{launch.details}\n-----------------------------------");
-        }
+        Console.WriteLine("Launches in 2011: "+ launch2011);
+
+
+        var successPerYear =
+            launches
+                //.Where(l => l.success == true)
+                .GroupBy(l => l.date_utc.Year)
+                .Select(lg => new
+                {
+                    Year = lg.Key,
+                    SuccessfulLaunches = lg.Count(l => l.success==true)
+                });
+
+        int i = 0;
+
+        // foreach (Launch launch in failures)
+        // {
+        //     Console.WriteLine($"{launch.name}\t\t{launch.date_utc.Year}\n" +
+        //                       $"{launch.details}\n-----------------------------------");
+        // }
     }
 }
